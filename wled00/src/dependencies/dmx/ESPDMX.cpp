2c61d47754be2e1bd0f8340220e878205e866896
Krzysiek Bielicki <krzysiek@skyman.pl>
2026-05-07 09:24:38 +0200

    change dmx output pin to 17

--- wled00/src/dependencies/dmx/ESPDMX.cpp
+++ wled00/src/dependencies/dmx/ESPDMX.cpp
@@ -28,7 +28,7 @@
 #define BREAKFORMAT    SERIAL_8N1
 
 bool dmxStarted = false;
-int sendPin = 2;		//default on ESP8266
+int sendPin = 17;		//default on ESP8266
 
 //DMX value array and size. Entry 0 will hold startbyte, so we need 512+1 elements
 uint8_t dmxDataStore[dmxMaxChannel+1] = {};
